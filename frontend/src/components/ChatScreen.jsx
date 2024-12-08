47c8702c284eaaa42dfabcc3854cc343625e3611
Maksym Ostanin <144963945+angaga2011@users.noreply.github.com>
2024-12-08 01:03:54 -0500

    Revert "Add contact added event listener and refresh contacts on JWT change"
    This reverts commit cc19dd8c5fff1afe2588bbb029505b9c3554c801.

--- frontend/src/components/ChatScreen.jsx
+++ frontend/src/components/ChatScreen.jsx
@@ -101,11 +101,6 @@ const ChatScreen = () => {
       }
     });
 
-    newSocket.on('contact_added', (data) => {
-      console.log('Contact added:', data);
-      fetchContacts(); // Refresh contacts list
-    });
-
     newSocket.on('message_saved', (data) => {
       if (data.success) {
         console.log('Message saved successfully');
@@ -120,10 +115,6 @@ const ChatScreen = () => {
       newSocket.disconnect();
     };
   }, [userEmail, selectedContact]);
-
-  useEffect(() => {
-    fetchContacts();
-  }, [jwt]);
 
   useEffect(() => {
     scrollToBottom();
@@ -141,6 +132,7 @@ const ChatScreen = () => {
     }
 
     try {
+
       if (!jwt) {
         alert("You are not logged in. Please log in first.");
         return;
@@ -243,11 +235,17 @@ const ChatScreen = () => {
         {/* Profile Section */}
         <div className="profile-section">
           <div className="profile">
+            {/* <img
+              src="https://via.placeholder.com/40"
+              alt="Your Profile"
+              className="profile-picture"
+            /> */}
+            
             <p className="profile-name">{userEmail}</p>
           </div>
           <button onClick={handleLogout} className="logout-button">
-            Logout 🔐 
-          </button>
+          Logout 🔐 
+        </button>
           <button className="settings-button" onClick={() => navigate("/settings")}>
             ⚙️
           </button>
@@ -264,8 +262,8 @@ const ChatScreen = () => {
         <div className="chat-messages">
           {memoizedMessages.map((message) => (
             <div
-              key={message.id}
-              className={`chat-message ${message.sender === userEmail ? "sent" : "received"}`}
+            key={message.id}
+            className={`chat-message ${message.sender === userEmail ? "sent" : "received"}`}
             >
               <p className="message-text">{message.content}</p>
               <p className="message-timestamp">{new Date(message.createdAt).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}</p>
